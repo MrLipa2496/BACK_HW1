33a491283eba5d68670ac7aab78fb6934b8c5788
MrLipa2496 <sasalipa216@gmail.com>
2024-09-03 17:55:50 +0300

    Complete basic implementation && Add requests

--- app.js
+++ app.js
@@ -31,3 +31,18 @@ const tasks = [
     isDone: false,
   },
 ];
+
+const getTask = (req, res) => {
+  res.status(200).send(tasks);
+};
+
+const createTask = (req, res) => {
+  const { body } = req;
+  tasks.push({ ...body, id: uuidv4() });
+  res.status(201).send(tasks);
+};
+
+app.get('/tasks', getTask);
+app.post('/tasks', createTask);
+
+module.exports = app;
